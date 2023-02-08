815b49efcdafbdd183a00be82316eaaa0f69dce3
ravissement <ravissement55@gmail.com>
2023-02-08 09:00:36 +0900

    fix : indentation

--- Egov/src/main/java/egovframework/example/cmmn/CmmnExcepHndlr.java
+++ Egov/src/main/java/egovframework/example/cmmn/CmmnExcepHndlr.java
@@ -34,7 +34,7 @@ import egovframework.rte.fdl.cmmn.exception.handler.ExceptionHandler;
  * @Description : EgovSampleExcepHndlr Class
  * @Modification Information
  * @
- * @  수정일			수정자			수정내용
+ * @ 수정일			수정자			수정내용
  * @ ---------		---------		-------------------------------
  * @ 2023.02.03		ravissement		최초생성
  *
